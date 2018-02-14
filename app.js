be76b950cb2a6fbda85236606cdfebc4b3cee08b
Veera Kishon Kumar Mucherla <veerakishon.mucherla@gmail.com>
2018-02-14 16:48:59 -0600

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var num1 = 0;
+var num2 = 3;
+console.log(num1+num2);
+function greeting(fn){
+console.log("In greeting");
+fn();
+}
+function hello(){
+    console.log("In hello function");
+}
+greeting(hello);
