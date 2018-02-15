5fca3c5231cb951f30647767e8de58be2405fb2a
KrishCR97 <veerakishon.mucherla@gmail.com>
2018-02-14 23:02:22 -0600

    Added chunks of code to greet/index.js

--- greet/index.js
+++ greet/index.js
@@ -1,6 +1,7 @@
 var english = require("./english.js");
 var spanish = require("./spanish.js");
-
+//Module exports statement runs only once as it will be cached for the second time.
+//To not to get the cached response we have to create a new object using new keyword.
 module.exports = {
     englis : english,
     spanis : spanish
