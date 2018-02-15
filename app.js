d6421df3230cc96b5e5374334d24e530b8ea32e2
KrishCR97 <veerakishon.mucherla@gmail.com>
2018-02-14 23:13:36 -0600

    Added greet.js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 let greet = require("./greet");
-var num1 = 0;
-var num2 = 3;
-console.log(num1+num2);
-function greeting(fn){
-console.log("In greeting");
-fn();
-}
-function hello(){
-    console.log("In hello function");
-}
-greeting(hello);
-module.exports = hello;
-greet.englis();
-greet.spanis();
+let greet1 = require("./greet.js");
+// var num1 = 0;
+// var num2 = 3;
+// console.log(num1+num2);
+// function greeting(fn){
+// console.log("In greeting");
+// fn();
+// }
+// function hello(){
+//     console.log("In hello function");
+// }
+// greeting(hello);
+// module.exports = hello;
+// greet.englis();
+// greet.spanis();
+greet1.num();
---
+++ greet.js
@@ -0,0 +1,5 @@
+exports.num = function(){
+    console.log("Used export in greet.js");
+}
+console.log(exports);
+console.log(module.exports);
