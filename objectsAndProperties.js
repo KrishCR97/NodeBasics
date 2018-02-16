876b8124f6f9681c7194232cd9266f39aadf3d04
KrishCR97 <veerakishon.mucherla@gmail.com>
2018-02-15 20:17:11 -0600

    Added objectsAndProperties.js file

---
+++ objectsAndProperties.js
@@ -0,0 +1,19 @@
+var obj = {
+    greet : "Hello Kishon"
+}
+console.log(obj['greet']);
+var arr = [];
+arr.push(function(){
+    console.log('Function1');
+});
+arr.push(function(){
+    console.log('Function2');
+});
+arr.forEach((item)=>{
+item();
+console.log('Yeah it worked');
+});
+//On is used to add a listener.
+function Emitter(){
+this.event = {};
+}
